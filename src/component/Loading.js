7af356ec558531679ef67b7f0be8a380d01d520b
friendlyanon <friendlyanon@users.noreply.github.com>
2020-12-27 15:07:02 +0100

    Add loading component

---
+++ src/component/Loading.js
@@ -0,0 +1,20 @@
+/**
+ * @format
+ */
+
+import React from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+
+const style = StyleSheet.create({
+  loading: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export const Loading = () => (
+  <View style={style.loading}>
+    <ActivityIndicator size="large" />
+  </View>
+);
