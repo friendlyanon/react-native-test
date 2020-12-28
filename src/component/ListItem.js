5dc5893c2a7fe0ea9a665f18a6c82415668e6aa7
friendlyanon <friendlyanon@users.noreply.github.com>
2020-12-28 18:53:35 +0100

    Implement list item component

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -3,8 +3,26 @@
  */
 
 import React from "react";
+import { StyleSheet, Pressable, Text } from "react-native";
 
-export const ListItem = () => (
-  <>
-  </>
+const style = StyleSheet.create({
+  wrapper: {
+    flexDirection: "column",
+  },
+  formatted: {
+    color: "black",
+  },
+  date: {
+    color: "gray",
+  },
+});
+
+export const ListItem = ({ item, navigation }) => (
+  <Pressable
+    style={style.wrapper}
+    onPress={() => navigation.navigate("Details", { id: item.id })}
+  >
+    <Text style={style.formatted}>{item.formatted}</Text>
+    <Text style={style.date}>{item.date}</Text>
+  </Pressable>
 );
