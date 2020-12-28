dbee94101658214ade67a13e8613d3928114f671
friendlyanon <friendlyanon@users.noreply.github.com>
2020-12-28 02:11:51 +0100

    Disable warning about require cycle

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
  */
 
 import "react-native-gesture-handler";
-import { AppRegistry } from "react-native";
+import { AppRegistry, LogBox } from "react-native";
 import { App } from "./App";
 import { name as appName } from "./app.json";
+
+LogBox.ignoreLogs([
+  // bogus warning about a require cycle
+  "Require cycle: node_modules\\react-native\\Libraries\\Network\\fetch.js",
+]);
 
 AppRegistry.registerComponent(appName, () => App);
